99e556fc7e953e1d9410b471f46313f1b580d5b2
KostyanWest <kostyanwest@gmail.com>
2021-10-27 21:51:13 +0300

    Lab07

---
+++ Sem5Lab07/BySemaphore/BySemaphore.cpp
@@ -0,0 +1,227 @@
+﻿// BySemaphore.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "BySemaphore.h"
+
+#define MAX_LOADSTRING 100
+
+#define IDC_EDIT     10001
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+HWND hEdit;
+HANDLE hSemaphore;
+HANDLE hThreads[3];
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass( HINSTANCE hInstance );
+BOOL                InitInstance( HINSTANCE, int );
+LRESULT CALLBACK    WndProc( HWND, UINT, WPARAM, LPARAM );
+INT_PTR CALLBACK    About( HWND, UINT, WPARAM, LPARAM );
+DWORD WINAPI        ThreadProc( LPVOID pParam );
+
+int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow )
+{
+    UNREFERENCED_PARAMETER( hPrevInstance );
+    UNREFERENCED_PARAMETER( lpCmdLine );
+
+    // Инициализация ресурсов
+    hSemaphore = CreateSemaphoreW( NULL, 1, 1, NULL );
+    hThreads[0] = CreateThread( NULL, 0, ThreadProc, (LPVOID)(L"String1"), CREATE_SUSPENDED, NULL );
+    hThreads[1] = CreateThread( NULL, 0, ThreadProc, (LPVOID)(L"String2"), CREATE_SUSPENDED, NULL );
+    hThreads[2] = CreateThread( NULL, 0, ThreadProc, (LPVOID)(L"String3"), CREATE_SUSPENDED, NULL );
+
+    // Инициализация глобальных строк
+    LoadStringW( hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING );
+    LoadStringW( hInstance, IDC_BYSEMAPHORE, szWindowClass, MAX_LOADSTRING );
+    MyRegisterClass( hInstance );
+
+    // Выполнить инициализацию приложения:
+    if (hSemaphore && hThreads[0] && hThreads[1] && hThreads[2] && !InitInstance( hInstance, nCmdShow ))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators( hInstance, MAKEINTRESOURCE( IDC_BYSEMAPHORE ) );
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage( &msg, nullptr, 0, 0 ))
+    {
+        if (!TranslateAccelerator( msg.hwnd, hAccelTable, &msg ))
+        {
+            TranslateMessage( &msg );
+            DispatchMessage( &msg );
+        }
+    }
+
+    // Освобождение ресурсов
+    TerminateThread( hThreads[2], 0 );
+    TerminateThread( hThreads[1], 0 );
+    TerminateThread( hThreads[0], 0 );
+    CloseHandle( hThreads[2] );
+    CloseHandle( hThreads[1] );
+    CloseHandle( hThreads[0] );
+    CloseHandle( hSemaphore );
+
+    return (int)msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass( HINSTANCE hInstance )
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize         = sizeof( WNDCLASSEX );
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon( hInstance, MAKEINTRESOURCE( IDI_BYSEMAPHORE ) );
+    wcex.hCursor        = LoadCursor( nullptr, IDC_ARROW );
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW( IDC_BYSEMAPHORE );
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon( wcex.hInstance, MAKEINTRESOURCE( IDI_SMALL ) );
+
+    return RegisterClassExW( &wcex );
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance( HINSTANCE hInstance, int nCmdShow )
+{
+    hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+    HWND hWnd = CreateWindowW( szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr );
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow( hWnd, nCmdShow );
+    UpdateWindow( hWnd );
+
+    return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
+{
+    switch (message)
+    {
+    case WM_CREATE:
+    {
+        hEdit = CreateWindowExW( WS_EX_CLIENTEDGE, L"EDIT", L"Edit",
+            WS_CHILD | WS_VISIBLE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
+            50, 50, 300, 30, hWnd, (HMENU)IDC_EDIT, hInst, nullptr );
+        for (int i = 0; i < 3; i++)
+        {
+            ResumeThread( hThreads[i] );
+        }
+    }
+    break;
+    case WM_COMMAND:
+    {
+        int wmId = LOWORD( wParam );
+        // Разобрать выбор в меню:
+        switch (wmId)
+        {
+        case IDM_ABOUT:
+            DialogBox( hInst, MAKEINTRESOURCE( IDD_ABOUTBOX ), hWnd, About );
+            break;
+        case IDM_EXIT:
+            DestroyWindow( hWnd );
+            break;
+        default:
+            return DefWindowProc( hWnd, message, wParam, lParam );
+        }
+    }
+    break;
+    case WM_DESTROY:
+        for (int i = 0; i < 3; i++)
+        {
+            SuspendThread( hThreads[i] );
+        }
+        PostQuitMessage( 0 );
+        break;
+    default:
+        return DefWindowProc( hWnd, message, wParam, lParam );
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About( HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam )
+{
+    UNREFERENCED_PARAMETER( lParam );
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD( wParam ) == IDOK || LOWORD( wParam ) == IDCANCEL)
+        {
+            EndDialog( hDlg, LOWORD( wParam ) );
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+DWORD WINAPI ThreadProc( LPVOID pParam )
+{
+    wchar_t* string = reinterpret_cast<wchar_t*>(pParam);
+    while (true)
+    {
+        auto result = WaitForSingleObject( hSemaphore, INFINITE );
+        switch (result)
+        {
+        case WAIT_OBJECT_0:
+        case WAIT_ABANDONED:
+            SetWindowTextW( hEdit, string );
+            Sleep( 1000 );
+            ReleaseSemaphore( hSemaphore, 1, nullptr );
+        default:
+            break;
+        }
+    }
+    return 0;
+}
